138e752105dc9ad67f8ef0ac2886dcb502d98fd5
Ethan-Edmond <eedmond@uccs.edu>
2021-06-16 14:37:31 -0600

    Finished deleteMovie functionality

--- client/src/components/DeleteMovieModal.js
+++ client/src/components/DeleteMovieModal.js
@@ -1,25 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
-const DeleteMovieModal = () => {
-    return (<div id="deleteMovieModal">
-        <div className="modal-dialog">
-            <div className="modal-content">
-                <form>
-                    <div className="modal-header">						
-                        <h4 className="modal-title">Delete Movie</h4>
-                        <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    </div>
-                    <div className="modal-body">					
-                        <p>Are you sure you want to delete these records?</p>
-                        <p className="text-warning"><small>This action cannot be undone.</small></p>
-                    </div>
-                    <div className="modal-footer">
-                        <input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel"/>
-                        <input type="submit" className="btn btn-danger" value="Delete"/>
-                    </div>
-                </form>
+const DeleteMovieModal = ({ cancelDelete, id, deleteMovie }) => {
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        deleteMovie(id);
+        window.location.href = '/movies';
+      })
+      .catch(alert);
+  };
+  return (
+    <div id="deleteMovieModal">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <form>
+            <div className="modal-header">						
+              <h4 className="modal-title">Delete Movie</h4>
+              <button
+                onClick={cancelDelete}
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-hidden="true"
+              >
+                &times;
+              </button>
             </div>
+            <div className="modal-body">					
+              <p>Are you sure you want to delete these records?</p>
+              <p className="text-warning"><small>This action cannot be undone.</small></p>
+            </div>
+            <div className="modal-footer">
+              <input
+                onClick={cancelDelete}
+                type="button"
+                className="btn btn-default"
+                data-dismiss="modal"
+                value="Cancel"
+              />
+              <input onClick={handleDelete} type="submit" className="btn btn-danger" value="Delete"/>
+            </div>
+          </form>
         </div>
+      </div>
     </div>)
 }
 
